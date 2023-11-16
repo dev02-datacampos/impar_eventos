f351b368d8435d0448e9a93a7585917b064a20eb
dev02 <dev02@datacampos.com>
2023-11-16 16:26:32 -0300

    first commit

--- script/index.js
+++ script/index.js
@@ -1,5 +1,15 @@
 document.querySelector(".hamburger").addEventListener("click", () =>
     document.querySelector(".container").classList.toggle("show-menu")
-);
+); //sidebar
 
-// commit
+function reiniciarVideo(video) {
+    video.currentTime = 0;
+    video.play();
+  }
+
+  var videos = document.querySelectorAll('.meu-video');
+  videos.forEach(function(video) {
+    video.addEventListener('ended', function() {
+      reiniciarVideo(video);
+    });
+  }); // reiniciar video autoplay
