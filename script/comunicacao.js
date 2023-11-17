b5acd9f57a3aa74e7c728d4fc7b99c828cad9e34
dev02 <dev02@datacampos.com>
2023-11-17 14:26:15 -0300

    segunda tela concluida

--- script/comunicacao.js
+++ script/comunicacao.js
@@ -1,3 +1,16 @@
 document.querySelector(".hamburger").addEventListener("click", () =>
     document.querySelector(".container").classList.toggle("show-menu")
 ); //sidebar
+
+var swiper = new Swiper(".swiper", {
+    cssMode: true,
+    loop: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+    keyboard: true,
+  }); //carrosel
