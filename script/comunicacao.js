7003a63a52cd0ba145e585d356663f3a6dabdd7e
dev02 <dev02@datacampos.com>
2023-11-21 10:40:26 -0300

    page sobre nos fin

--- script/comunicacao.js
+++ script/comunicacao.js
@@ -1,4 +1,3 @@
 document.querySelector(".hamburger").addEventListener("click", () =>
     document.querySelector(".container").classList.toggle("show-menu")
 ); //sidebar
-
---
+++ script/sobre.js
@@ -0,0 +1,15 @@
+function reiniciarVideo(video) {
+    video.currentTime = 0;
+    video.play();
+  }
+
+  var videos = document.querySelectorAll('.meu-video');
+  videos.forEach(function(video) {
+    video.addEventListener('ended', function() {
+      reiniciarVideo(video);
+    });
+  }); // reiniciar video autoplay
+
+  document.querySelector(".hamburger").addEventListener("click", () =>
+  document.querySelector(".container").classList.toggle("show-menu")
+); //sidebar
